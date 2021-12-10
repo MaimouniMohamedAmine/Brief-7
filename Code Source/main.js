810b416cc608fc2c9076b23f55c3d56c969f2818
MaimouniMohamedAmine <92027914+MaimouniMohamedAmine@users.noreply.github.com>
2021-12-10 16:53:57 +0100

    need work on Edit btn

--- Code Source/main.js
+++ Code Source/main.js
@@ -17,7 +17,7 @@ function validateForm(e){
     
 
     // Tester si les champs sont vides
-    for(var i=0;i<inputs.length-4; i++){
+    for(var i=0;i<4; i++){
         if(inputs[i].value == ""){
             validationErrors++;
             inputs[i].nextElementSibling.style.color = "red";
@@ -36,7 +36,7 @@ function validateForm(e){
             inputs[0].nextElementSibling.style.color = "red";
             inputs[0].nextElementSibling.innerHTML = "Ce n'est pas un titre!";
         }
-        else if(isNaN(inputs[0].value) && inputs[0].value.trim().length > 30)
+        else if(!isNaN(inputs[0].value) && inputs[0].value.trim().length > 30)
         {
             inputs[0].nextElementSibling.style.color = "red";
             validationErrors++;
@@ -74,7 +74,7 @@ function validateForm(e){
             inputs[2].nextElementSibling.style.color = "green";
             inputs[2].nextElementSibling.innerHTML = "Champ valide";
         }
-        else if(!isNaN(inputs[2].value) && inputs[2].value < 0) {
+        else if(isNaN(inputs[2].value) && inputs[2].value < 0) {
             validationErrors++;
             inputs[2].nextElementSibling.style.color = "red";
             inputs[2].nextElementSibling.innerHTML = "Le prix doit être positif";
@@ -109,6 +109,7 @@ function validateForm(e){
 
         ///////////-Tableau/insertCell-///////////////
 
+        
         if(validationErrors==0){
             var newRow = table.insertRow(-1);
             var cell1 = newRow.insertCell(0);
@@ -135,10 +136,68 @@ function validateForm(e){
                 cell4.innerHTML = inputs[3].value;
                 cell5.innerHTML = select.options[select.selectedIndex].value;
                 cell6.innerHTML = CellType;
-                newRow.insertCell(6).innerHTML = "<input type='button' value='Modifier'><input type='button' value='Supprimer'>";
-            }
+                newRow.insertCell(6).innerHTML = "<input onClick='Edit(this)' type='button' value='Modifier'><input type='button' value='Supprimer' onClick = 'Delete(this)'>";
+                                                 //`<a onClick="onEdit(this)">Edit</a>
+                                                 //<a onClick="onDelete(this)">Delete</a>`;                      
+                
+                
+                
+            
+    
+        }
     }
-document.getElementsByTagName("form")[0].addEventListener("submit", validateForm);
-// couldn't get it to work with the previous version
-//I had to call the form with its TagName instead of its Id 
-//Zoubair helped me to get it back   
+        function Delete(td){
+            if (confirm('Vous êtes sûre de supprimer cette entrée?')) {
+            row = td.parentElement.parentElement;
+            document.getElementById("table").deleteRow(row.rowIndex);
+
+        }}
+
+        // Edit function;
+        function Edit(r){
+            var i = r.parentNode.parentNode.rowIndex;
+            var row = table.rows[i];
+            if(r.value == "Edit"){
+                inputs[0].value = row.cells[0].innerHTML;
+                inputs[1].value = row.cells[1].innerHTML;
+                inputs[2].value = row.cells[2].innerHTML;
+                inputs[3].value = row.cells[3].innerHTML;
+                
+                // Language
+                if(row.cells[4].innerHTML == "Anglais"){
+                    lang.selectedIndex = 1;
+                }
+                else if(row.cells[4].innerHTML == "English"){
+                    lang.selectedIndex = 2;
+                }
+                else{
+                    lang.selectedIndex = 3;
+                }
+
+                // Type
+                for(var i=0;i<type.length;i++){
+                    if(row.cells[5].innerHTML==type[i].value){
+                        type[i].checked = true;
+                    }
+                }
+                r.value="Save"
+                document.getElementById("Soumettre").setAttribute("disabled","true");
+            }
+            else{
+                row.cells[0].innerHTML = title.value;
+                row.cells[1].innerHTML = author.value;
+                row.cells[2].innerHTML = price.value;
+                row.cells[3].innerHTML = pub.value;
+                row.cells[4].innerHTML = lang.options[lang.selectedIndex].value;
+                for(var i=0;i<type.length;i++){
+                    if(type[i].checked){
+                        row.cells[5] = type[i].value;
+                    }
+                }
+                r.value = "Edit";
+                document.getElementById("Soumettre").removeAttribute("disabled")
+            }     }    
+        document.getElementsByTagName("form")[0].addEventListener("submit", validateForm);
+            // couldn't get it to work with the previous version
+            //I had to call the form with its TagName instead of its Id 
+            //Zoubair helped me to get it back   
